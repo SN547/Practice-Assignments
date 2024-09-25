7abf2db6b9bd96474b7d6f3aeaff75bd702d20d1
Shraddha Naik <81643089+SN547@users.noreply.github.com>
2024-09-25 10:29:19 +0530

    Add files via upload

--- Assignment_1.java
+++ Assignment_1.java
@@ -1,30 +1,48 @@
-package module_2;
+package module_4;
 
-import java.util.List;
-
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Assignment_1 {
 
 	public static void main(String[] args) throws InterruptedException {
-		// web table
+		//launch browser
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://demo.guru99.com/test/web-table-element.php");
+		driver.get("http://demo.guru99.com/V4/");
 		driver.manage().window().maximize();
-		Thread.sleep(2000);	    
 		
-        List<WebElement> companyNames = driver.findElements(By.xpath("//table[@class='dataTable']//tbody//tr//td[1]"));
-				
-		System.out.println("Company names in the Web Table: ");
-		
-		for(WebElement companyName : companyNames) {
-			System.out.println(companyName.getText());
-		}
-        
-        driver.quit();
 
-}
+		// get data from excel file
+		Xls_Reader reader = new Xls_Reader(
+				"C://Users//Bunty//Desktop//Eclipse//SeleniumBatch_Aug2024//src//main//java//com//testdata//UserPass.xlsx");
+		int rowCount = reader.getRowCount("Sheet1");
+		
+		//parameterization
+		for (int rowNum = 2; rowNum <= rowCount; rowNum++) {
+			
+			String userName = reader.getCellData("Sheet1", "username", rowNum);
+			System.out.println(userName);
+
+			String passWord = reader.getCellData("Sheet1", "password", rowNum);
+			System.out.println(passWord);
+			
+			//enter data
+			driver.findElement(By.xpath("//input[@name='uid']")).clear();
+			driver.findElement(By.xpath("//input[@name='uid']")).sendKeys("userName");
+			driver.findElement(By.xpath("//input[@name='password']")).clear();
+			driver.findElement(By.xpath("//input[@name='password']")).sendKeys("passWord");
+			driver.findElement(By.xpath("//input[@name='btnLogin']")).click();
+			
+			Alert a = driver.switchTo().alert();
+			Thread.sleep(1000);
+			//to close alert
+			a.accept();
+			
+
+		}
+			driver.quit();
+	}
+
 }
